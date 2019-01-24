2031c78aa76b254422952a18d49906ba041ef05a
Kiersten <kschutz@gatech.edu>
2019-01-23 22:48:02 -0500

    Edited p2 file

--- M2/src/main/java/edu/gatech/oad/antlab/person/Person2.java
+++ M2/src/main/java/edu/gatech/oad/antlab/person/Person2.java
@@ -1,11 +1,12 @@
 package edu.gatech.oad.antlab.person;
+import java.util.Random;
 
 /**
  *  A simple class for person 2
  *  returns their name and a
- *  modified string 
+ *  modified string
  *
- * @author Bob
+ * @author Kiersten Schutz
  * @version 1.1
  */
 public class Person2 {
@@ -31,14 +32,25 @@ public class Person2 {
 	 */
 	private String calc(String input) {
 	  //Person 2 put your implementation here
-	  return null;
+		String output = null;
+		Random rand = new Random();
+		String used = new String();
+		while (used.length() < input.length()) {
+			int value = rand.nextInt();
+			char c = input.charAt(value);
+			if (!used.contains(Character.toString(c))) {
+				output = output + c;
+				used = used + c;
+			}
+		}
+	 	return output;
 	}
 	/**
 	 * Return a string rep of this object
 	 * that varies with an input string
 	 *
 	 * @param input the varying string
-	 * @return the string representing the 
+	 * @return the string representing the
 	 *         object
 	 */
 	public String toString(String input) {
